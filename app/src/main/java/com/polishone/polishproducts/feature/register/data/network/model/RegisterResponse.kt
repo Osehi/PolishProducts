c6038e778e490e13e5bd5c5bc47247f1c62a714e
Osehi <osehiaseehilen@gmail.com>
2022-07-08 14:08:35 +0100

    register-network-module-is-populated

---
+++ app/src/main/java/com/polishone/polishproducts/feature/register/data/network/model/RegisterResponse.kt
@@ -0,0 +1,5 @@
+package com.polishone.polishproducts.feature.register.data.network.model
+
+data class RegisterResponse(
+    val message: String?
+)
--- app/src/main/java/com/polishone/polishproducts/presentation/MainActivity.kt
+++ app/src/main/java/com/polishone/polishproducts/presentation/MainActivity.kt
@@ -3,7 +3,9 @@ package com.polishone.polishproducts.presentation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.polishone.polishproducts.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
