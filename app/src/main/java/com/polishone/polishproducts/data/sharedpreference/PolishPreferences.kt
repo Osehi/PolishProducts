0c77e1757b124fc7b8c8641cd5ba52b89b1ef12a
Osehi <osehiaseehilen@gmail.com>
2022-08-18 09:09:39 +0100

    Added sharedPreference

--- app/src/main/java/com/polishone/polishproducts/data/sharedpreference/PolishPreferences.kt
+++ app/src/main/java/com/polishone/polishproducts/data/sharedpreference/PolishPreferences.kt
@@ -6,14 +6,16 @@ import javax.inject.Inject
 class PolishPreferences @Inject constructor(context: Context) : Preferences {
 
     companion object {
-        const val PREFERENCES_NAME = "DECAPAY_PREFERENCES"
+        const val PREFERENCES_NAME = "POLISH_PREFERENCES"
     }
 
+    private val prefences = context.getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE)
+
     override fun putToken(token: String) {
-        //
+        prefences.edit().putString(PreferenceConstant.KEY_TOKEN, token).apply()
     }
 
     override fun getToken(): String {
-        //
+        return prefences.getString(PreferenceConstant.KEY_TOKEN, "").orEmpty()
     }
 }
