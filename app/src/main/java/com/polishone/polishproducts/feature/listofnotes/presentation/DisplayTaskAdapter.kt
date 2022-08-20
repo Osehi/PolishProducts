a6c2dd8c50c5c182e729c5da4c4e6d4c6468f4eb
Osehi <osehiaseehilen@gmail.com>
2022-08-20 20:40:42 +0100

    Added bind function

--- app/src/main/java/com/polishone/polishproducts/feature/listofnotes/presentation/DisplayTaskAdapter.kt
+++ app/src/main/java/com/polishone/polishproducts/feature/listofnotes/presentation/DisplayTaskAdapter.kt
@@ -2,13 +2,15 @@ package com.polishone.polishproducts.feature.listofnotes.presentation
 
 import androidx.recyclerview.widget.RecyclerView
 import com.polishone.polishproducts.databinding.CreateNoteItemBinding
+import com.polishone.polishproducts.feature.listofnotes.data.model.Note
 
 class DisplayTaskAdapter {
 
-    class DisplayTaskViewHolder(private val binding: CreateNoteItemBinding): RecyclerView.ViewHolder(binding.root) {
-        fun bind() {
+    class DisplayTaskViewHolder(private val binding: CreateNoteItemBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(task: Note) {
             // initialize the views
+            binding.createNoteItemTitleTv.text = task.title
+            binding.createNoteItemContentTv.text = task.content
         }
-
     }
 }
